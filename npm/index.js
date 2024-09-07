79989c3cc68965a57fbf5e7b363a28234d45f4ac
Petrik <112963523+petrikpopov@users.noreply.github.com>
2024-09-07 16:28:27 +0300

    first commit

---
+++ npm/index.js
@@ -0,0 +1,4 @@
+// const Person = require("./person");
+import Person from "./person.js";
+const person =  new Person("Petro", 20, 86);
+console.log(person.showInfo())
---
+++ text-myNpm/index.js
@@ -0,0 +1,4 @@
+// const Person = require("/Users/popov911/Desktop/NodeJS/text-myNpm/node_modules/popov-front-end/person.js");
+import Person from "/node_modules/popov-front-end/person.js"
+const person =  new Person("Ivanel", 24, 96);
+console.log(person.showInfo())
