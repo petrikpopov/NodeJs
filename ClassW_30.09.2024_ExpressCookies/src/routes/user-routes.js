ce4977447e4921a096c40441462462b6c88e0ccc
Petrik <112963523+petrikpopov@users.noreply.github.com>
2024-10-12 23:47:15 +0300

    created HomeW JSON Web Token

--- ClassW_30.09.2024_ExpressCookies/src/routes/user-routes.js
+++ ClassW_30.09.2024_ExpressCookies/src/routes/user-routes.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
 import validator from "validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { Users } from "../data/users.js";
 
 const userRoutes = Router();
+const secretAccessKey = process.env.ACCESS_SECRET_KEY;
+const secretRefreshKey = process.env.REFRESH_SECRET_KEY;
+const accessTokenExpiration = "15m"; 
+const refreshTokenExpiration = "7d"; 
+
+const refreshTokens = [];
+
+function generateAccessToken(user) {
+    return jwt.sign(user, secretAccessKey, { expiresIn: accessTokenExpiration });
+}
+
+function generateRefreshToken(user) {
+    const refreshToken = jwt.sign(user, secretRefreshKey, { expiresIn: refreshTokenExpiration });
+    refreshTokens.push(refreshToken);
+    return refreshToken;
+}
 
 userRoutes.get('/', (req, res)=>{
     res.json(Users);
@@ -14,12 +31,6 @@ userRoutes.route('/signup')
     .get((req, res)=>{
         res.render("form_register");})
     .post((req, res)=>{
-        // console.log(req.body);
-        // console.log(validator.isEmail(req.body.email));
-        // console.log(bcrypt.hashSync(req.body.password, 10)); //хеш
-        // bcrypt.compareSync();
-
-        //HomeWork
         if(!req.body.login || !req.body.email || !req.body.password || !req.body.confirm_password) {
             return res.status(400).send('Все поля обязательны для заполнения.');
         }
@@ -32,6 +43,7 @@ userRoutes.route('/signup')
         if(req.body.password !== req.body.confirm_password) {
             return res.status(400).send('Пароли не совпадают.');
         }
+
         const hashedPassword = bcrypt.hashSync(req.body.password, 10);
 
         console.log({
@@ -49,10 +61,15 @@ userRoutes.route('/signup')
 
         console.log(Users);
 
-        req.session.user = {
-            login: req.body.login,
-            email: req.body.email,
-        }
+        // req.session.user = {
+        //     login: req.body.login,
+        //     email: req.body.email,
+        // }
+        const accessToken = generateAccessToken({ login: user.login });
+        const refreshToken = generateRefreshToken({ login: user.login });
+
+        res.json({ accessToken, refreshToken });
+
         res.redirect('/')
     });
 
@@ -70,10 +87,15 @@ userRoutes.route('/signin')
         const {login, password} = req.body;
         const user = Users.find(el => el.login === login);
         if (user) {
-            req.session.user = {
-                login: user.login
-            };
-            console.log("User found:", user);
+
+            const accessToken = generateAccessToken({ login: user.login });
+            const refreshToken = generateRefreshToken({ login: user.login });
+
+            res.json({ accessToken, refreshToken });
+            // req.session.user = {
+            //     login: user.login
+            // };
+            // console.log("User found:", user);
             
             res.redirect('/');
         } else {
@@ -86,5 +108,37 @@ userRoutes.route('/signin')
             password: req.body.password
         });
     });
+
+userRoutes.post('/token', (req, res) => {
+    const { token } = req.body;
+    if (!token) return res.sendStatus(401);
+
+    if (!refreshTokens.includes(token)) return res.sendStatus(403);
+
+    jwt.verify(token, secretRefreshKey, (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        const accessToken = generateAccessToken({ login: user.login });
+        res.json({ accessToken });
+    });
+});
+
+userRoutes.get('/list_of_users', authenticateToken, (req, res)=>{
+    let existUser = Users?.length > 0 ? 1 : 0;
+    res.render("list_of_users", {Users, existUser});
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, secretAccessKey, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next(); 
+    });
+}
 
 export default userRoutes
